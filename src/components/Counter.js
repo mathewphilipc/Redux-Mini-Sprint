49bfe1b4af6ea2d62c6381b691b98e2b8cd871d3
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-03 21:26:10 -0400

    Got everything working

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -21,11 +21,11 @@ class Counter extends Component {
             <p>
                 Clicked: {this.props.count} times
                 {" "}
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => this.props.increment()}>
                     +
                 </button>
                 {" "}
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => this.props.decrement()}>
                     -
                 </button>
                 {" "}
@@ -49,6 +49,7 @@ class Counter extends Component {
 // this component receives the whole state. In a more complex
 // redux application, though, it would receive only the relevant
 // parts it needs from the state object.
+
 const mapStateToProps = (state) => {
     return {
         count: state
@@ -60,4 +61,5 @@ const mapStateToProps = (state) => {
 // is only a dumb React component. We pass in all of the functions that
 // are reliant on Redux, along with the component itself, so that Redux
 // makes itself known to this component.
+
 export default connect(mapStateToProps, { increment, decrement })(Counter);
